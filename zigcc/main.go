503553c94d07e9745f176ffd0ee85f07586337dc
dosgo <dosgo@qq.com>
2021-08-19 12:01:03 +0800

    fix build bug

---
+++ zigcc/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os/exec"
+	"fmt"
+	"github.com/dosgo/zigtool/comm"
+)
+
+func main()  {
+	_,err:=exec.LookPath("zig")
+	if err!=nil{
+		fmt.Println("Zig is not installed or not added to the path environment variable")
+		return
+	}
+	comm.Build("cc")
+}
